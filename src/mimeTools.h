325445be65a59d67a60c963b24413e56ef323312
Don Ho <don.h@free.fr>
2022-11-06 13:35:46 +0100

    MimeTools 2.9 release

--- src/mimeTools.h
+++ src/mimeTools.h
@@ -21,8 +21,8 @@
 #ifndef NPP_TOOLS_H
 #define NPP_TOOLS_H
 
-#define VERSION_VALUE "2.8\0"
-#define VERSION_DIGITALVALUE 2, 8, 0, 0
+#define VERSION_VALUE "2.9\0"
+#define VERSION_DIGITALVALUE 2, 9, 0, 0
 
 #define IDD_ABOUTBOX 250
 
