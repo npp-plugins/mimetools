704923f6108abf61699d730d095e71e4aae35a89
Don Ho <don.h@free.fr>
2023-12-18 19:23:04 +0100

    Add URL Encode Extended command
    Current "URL Encode" command of which the implementation follows RFC 1738 is renameed to "URL Encode (RFC 1738)".
    The new added command "URL Encode (Extended)" follows the most popular implementation which encode also the following characters "$-_.+!*'(),".
    
    Fix #28

--- src/url.cpp
+++ src/url.cpp
@@ -16,25 +16,85 @@
 
 
 #include <string.h>
+#include <string>
 #include <ctype.h>
 
 #include "url.h"
 
-// These characters must be encoded in a URL, as per RFC1738
-static const char gReservedAscii[] = "<>\"#%{}|\\^~[]`;/?:@=& ";  
+
+// Unsafe:
+// 
+// Characters can be unsafe for a number of reasons.The space
+// character is unsafe because significant spaces may disappearand
+// insignificant spaces may be introduced when URLs are transcribed or
+// typeset or subjected to the treatment of word - processing programs.
+// The characters "<" and ">" are unsafe because they are used as the
+// delimiters around URLs in free text; the quote mark(""") is used to
+//     delimit URLs in some systems.The character "#" is unsafe and should
+//     always be encoded because it is used in World Wide Web and in other
+//     systems to delimit a URL from a fragment / anchor identifier that might
+//     follow it.The character "%" is unsafe because it is used for
+//     encodings of other characters.Other characters are unsafe because
+//     gatewaysand other transport agents are known to sometimes modify
+//     such characters.These characters are "{", "}", "|", "\", " ^ ", "~",
+//     "[", "]", and "`".
+// 
+//     All unsafe characters must always be encoded within a URL.For
+//     example, the character "#" must be encoded within URLs even in
+//     systems that do not normally deal with fragment or anchor
+//     identifiers, so that if the URL is copied into another system that
+//     does use them, it will not be necessary to change the URL encoding.
+//
+// Reserved:
+//
+// Many URL schemes reserve certain characters for a special meaning :
+// their appearance in the scheme - specific part of the URL has a
+// designated semantics.If the character corresponding to an octet is
+// reserved in a scheme, the octet must be encoded.The characters ";",
+// "/", "?", ":", "@", "=" and "&" are the characters which may be
+// reserved for special meaning within a scheme.No other characters may
+// be reserved within a scheme.
+//
+// Usually a URL has the same interpretation when an octet is
+// represented by a characterand when it encoded.However, this is not
+// true for reserved characters : encoding a character reserved for a
+// particular scheme may change the semantics of a URL.
+// 
+// Thus, only alphanumerics, the special characters "$-_.+!*'(),", and
+// reserved characters used for their reserved purposes may be used
+// unencoded within a URL.
+// 
+// On the other hand, characters that are not required to be encoded
+// (including alphanumerics) may be encoded within the scheme - specific
+// part of a URL, as long as they are not being used for a reserved
+// purpose.
+//
+// https://datatracker.ietf.org/doc/html/rfc1738
+
+// These "unsafe" characters must be encoded in a URL, as per RFC1738
+static const char gReservedAscii[] = "<>\"#%{}|\\^~[]`;/?:@=& ";
+
+// In order to follow the "standard" implementations,
+// the following characters which are allowed to not be encoded (according RFC 1738)
+// can be included into "must be encoded" characters.
+static const char gExtendedChar[] = "!*'()+$,";
+
 static const char gHexChar[] = "0123456789ABCDEF";
 
-int AsciiToUrl (char* dest, const char* src, int destSize, bool encodeAll)
+int AsciiToUrl (char* dest, const char* src, int destSize, UrlEncodeMethod method)
 {
   int i;
-
   memset (dest, 0, destSize);
+  std::string reservedAscii = gReservedAscii;
+  
+  if (method == UrlEncodeMethod::extended)
+      reservedAscii += gExtendedChar;
 
   for (i = 0; (i < (destSize - 2)) && *src; ++i, ++src)
   {
     // Encode source if it is a reserved or non-printable character.
     //
-    if (encodeAll || (strchr (gReservedAscii, *src) != 0) || !isprint(*src))
+    if (method == UrlEncodeMethod::full || (strchr (reservedAscii.c_str(), *src) != 0) || !isprint(*src))
     {
       *dest++ = '%';
       *dest++ = gHexChar [((*src >> 4) & 0x0f)];
